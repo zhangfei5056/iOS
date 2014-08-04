4b09223a9e16bdccc390dff7d7fead9d5ddede8f
caoyuan <caoyuan@MacBookAir.local>
2014-08-04 15:09:25 +0800

    test auto layout

---
+++ test/testAutoLayout/testAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testAutoLayout
+//
+//  Created by caoyuan on 7/24/14.
+//  Copyright (c) 2014 caoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
