ede5eb9f0cdba412a7c412439d699b2598b5ba1d
caoyuan <caoyuan@MacBookAir.local>
2014-08-19 02:06:01 +0800

    add tableview refreshing
    drag up add more
    drag down refreshing

---
+++ test/testTableViewRefresh/testTableViewRefresh/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  testTableViewRefresh
+//
+//  Created by caoyuan on 8/19/14.
+//  Copyright (c) 2014 cniia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
---
+++ test/testTableViewRefresh/testTableViewRefresh/ViewController.m
@@ -0,0 +1,144 @@
+//
+//  ViewController.m
+//  testTableViewRefresh
+//
+//  Created by caoyuan on 8/19/14.
+//  Copyright (c) 2014 cniia. All rights reserved.
+//
+/*
+ 任何tableView想要添加下拉刷新，上啦加载更多，仅需要添加MJRefresh.h头文件即可，然后
+ 里面在需要得tableview得表头，按照本页面得得方法写即可。
+ */
+#import "ViewController.h"
+#import "MJRefresh.h"
+
+
+NSString *const MJTableViewCellIdentifier = @"Cell";
+
+/**
+ *  随机数据
+ */
+#define MJRandomData [NSString stringWithFormat:@"随机数据---%d", arc4random_uniform(1000000)]
+
+
+
+@interface ViewController ()
+@property (strong, nonatomic) NSMutableArray *fakeData;
+@end
+
+@implementation ViewController
+#pragma mark - 初始化
+/**
+ *  数据的懒加载
+ */
+- (NSMutableArray *)fakeData
+{
+    if (!_fakeData) {
+        self.fakeData = [NSMutableArray array];
+
+        for (int i = 0; i<12; i++) {
+            [self.fakeData addObject:MJRandomData];
+        }
+    }
+    return _fakeData;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    // 1.注册cell
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:MJTableViewCellIdentifier];
+
+    // 2.集成刷新控件
+    [self setupRefresh];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+/**
+ *  集成刷新控件
+ */
+- (void)setupRefresh
+{
+    // 1.下拉刷新(进入刷新状态就会调用self的headerRereshing)
+    [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing)];
+#warning 自动刷新(一进入程序就下拉刷新)
+    [self.tableView headerBeginRefreshing];
+
+    // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
+    [self.tableView addFooterWithTarget:self action:@selector(footerRereshing)];
+
+    // 设置文字(也可以不设置,默认的文字在MJRefreshConst中修改)
+    self.tableView.headerPullToRefreshText = @"下拉可以刷新了";
+    self.tableView.headerReleaseToRefreshText = @"松开马上刷新了";
+    self.tableView.headerRefreshingText = @"正在刷新中...";
+
+    self.tableView.footerPullToRefreshText = @"上拉可以加载更多数据";
+    self.tableView.footerReleaseToRefreshText = @"松开马上加载更多数据";
+    self.tableView.footerRefreshingText = @"正在加载中...";
+}
+
+#pragma mark 开始进入刷新状态
+- (void)headerRereshing
+{
+    // 1.添加假数据
+    for (int i = 0; i<5; i++) {
+        [self.fakeData insertObject:MJRandomData atIndex:0];
+    }
+
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView headerEndRefreshing];
+    });
+}
+
+- (void)footerRereshing
+{
+    // 1.添加假数据
+    for (int i = 0; i<5; i++) {
+        [self.fakeData addObject:MJRandomData];
+    }
+
+    // 2.2秒后刷新表格UI
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView footerEndRefreshing];
+    });
+}
+
+#pragma mark - Table view data source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.fakeData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MJTableViewCellIdentifier forIndexPath:indexPath];
+
+    cell.textLabel.text = self.fakeData[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    MJTestViewController *test = [[MJTestViewController alloc] init];
+//    [self.navigationController pushViewController:test animated:YES];
+}
+@end
